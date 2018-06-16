f8d9462149079a052b4951ad6be36d7a0e3d2df9
Aleksey <alekseytimoshchenko@gmail.com>
2018-06-16 12:16:40 +0300

    added movie fragment

--- app/src/main/java/com/example/admin/thebestapp/ui/movie/MovieActivity.kt
+++ app/src/main/java/com/example/admin/thebestapp/ui/movie/MovieActivity.kt
@@ -1,15 +1,20 @@
 package com.example.admin.thebestapp.ui.movie
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.admin.thebestapp.R
 
 class MovieActivity: AppCompatActivity()
 {
-    
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie)
+    }
+    
+    override fun onConfigurationChanged(newConfig: Configuration?)
+    {
+        super.onConfigurationChanged(newConfig)
     }
 }
--- app/src/main/java/com/example/admin/thebestapp/ui/movie/movieFragment/mvp/MovieFragment.kt
+++ app/src/main/java/com/example/admin/thebestapp/ui/movie/movieFragment/mvp/MovieFragment.kt
@@ -1,10 +1,21 @@
 package com.example.admin.thebestapp.ui.movie.movieFragment.mvp
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.admin.thebestapp.R
 
 class MovieFragment: Fragment(), MovieContract.View
 {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+        var rootView = inflater.inflate(R.layout.frag_movie, container, false)
     
+    
+        return rootView
+    }
     
     override fun setContent()
     {
