4392ebcf10149fc59747507ba9e02eda1e3e6ca6
Aleksey <alekseytimoshchenko@gmail.com>
2018-06-17 08:09:57 +0300

    added progress indicator

--- app/src/main/java/com/example/admin/thebestapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/admin/thebestapp/ui/splash/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.admin.thebestapp.R
+import com.example.admin.thebestapp.app.extensions.ProgressIndicator
 import com.example.admin.thebestapp.ui.movie.MovieActivity
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -22,8 +23,10 @@ class SplashActivity: AppCompatActivity()
     private fun startSplash()
     {
         Observable.just("")//
+                .doOnNext { ProgressIndicator(this).setProgressDialog(true, window) }
                 .delay(2000, TimeUnit.MILLISECONDS)//
                 .observeOn(AndroidSchedulers.mainThread())//
+                .doOnNext { ProgressIndicator(this).setProgressDialog(false, window) }
                 .subscribe({ goToMovieActivity() })
     }
     
